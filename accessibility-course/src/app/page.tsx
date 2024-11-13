36b4b75990e0ad38f9588ae237e3700c1a374136
Ashraf, Areeb <areebashraf@my.unt.edu>
2024-11-13 12:56:04 -0600

    Base next.js layout

---
+++ accessibility-course/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+// how to import:
+// <Image
+//   aria-hidden
+//   src="/window.svg"
+//   alt="Window icon"
+//   width={16}
+//   height={16}
+// />
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      This is the base for the Accessibility course app using next.js
+    </>
+  );
+}
