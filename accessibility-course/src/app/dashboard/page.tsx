4bea1c4975852adfa577945688730e820db0c665
Ashraf, Areeb <areebashraf@my.unt.edu>
2024-12-01 17:36:19 -0600

    Dashboard Frontend Layout + Responsiveness

--- accessibility-course/src/app/dashboard/page.tsx
+++ accessibility-course/src/app/dashboard/page.tsx
@@ -1,31 +1,61 @@
-import React from 'react'
+import React from "react";
 import { auth, signOut } from "@/auth";
+import "../styles/dashboard.css";
 
 export default async function Dashboard() {
-    const session = await auth();
+  const session = await auth();
   return (
     <>
-      Dashboard
-      <h3 className="text-2xl font-semibold">
-        User session data:
-      </h3>
       {session ? (
-        <div>
-          <pre>
-            {JSON.stringify(session, null, 2)}
-          </pre>
-          <form
-            action={async () => {
-              "use server";
-              await signOut({ redirectTo: '/login' });
-            }}
-          >
-           <button type='submit'> signOut</button>
-          </form>
+        <div className="dashboard-container">
+          <div className="dashboard-left">
+            <div className="dashboard-panel"></div>
+            <div className="dashboard-middle">
+              <div className="dashboard-todo">
+                To Do
+                <div className="todo-card-container">
+                  <div className="todo-card"></div>
+                  <div className="todo-card"></div>
+                </div>
+              </div>
+              <div className="dashboard-recent-scores">
+                Recent Scores
+                <div className="recent-score-chart-container">
+                  <pre>{JSON.stringify(session, null, 2)}</pre>
+                  <form
+                    action={async () => {
+                      "use server";
+                      await signOut({ redirectTo: "/login" });
+                    }}
+                  >
+                    <button type="submit"> signOut</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="dashboard-bottom">
+            <div className="dashboard-achievements">
+              Achievements
+              <div className="badges-container"></div>
+            </div>
+            <div className="dashboard-course-progress">
+              Course Progress
+              <div className="progress-circles-container"></div>
+            </div>
+            </div>
+          </div>
+          <div className="dashboard-right">
+          <div className="recent-notifications"></div>
+          <div className="stats-container">
+            <div className="xp-container"></div>
+            <div className="lvl-container"></div>
+          </div>
+          <div className="top-performers"></div>
+          </div>
         </div>
       ) : (
         <div>Not signed in</div>
       )}
     </>
-  )
+  );
 }
