84e658442142e30f2ba2a6e81fa7c43ba21516ae
Ashraf, Areeb <areebashraf@my.unt.edu>
2025-03-16 23:02:28 -0500

    homepage

--- accessibility-course/src/app/page.tsx
+++ accessibility-course/src/app/page.tsx
@@ -1,18 +1,28 @@
-import Image from "next/image";
-// how to import:
-// <Image
-//   aria-hidden
-//   src="/window.svg"
-//   alt="Window icon"
-//   width={16}
-//   height={16}
-// />
-import styles from "./page.module.css";
+import "./styles/homepage.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      This is the base for the Accessibility course app using next.js
-    </>
+    <div className="homepage-container">
+      <main className="hero-section">
+        <img src="/sitting-panda.svg" alt="sitting panda" />
+        <div>
+          <h1 className="hero-title">Learn Accessibility</h1>
+          <p className="hero-description">
+            Master the art of creating inclusive digital experiences that work for everyone. 
+            This course will equip you with the knowledge and skills to make your web applications 
+            accessible to users of all abilities.
+          </p>
+          <div className="cta-buttons">
+            <Link href="/login" className="btn btn-primary">
+              Login
+            </Link>
+            <Link href="/signup" className="btn btn-secondary">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
